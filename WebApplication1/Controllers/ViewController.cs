06aff755b7b08b32fb7705cfa78520e8508787be
Ngocquyentran <quyentrthng@gmail.com>
2024-06-18 14:00:51 +0700

    first commit

---
+++ WebApplication1/Controllers/ViewController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class ViewController : Controller
+    {
+        private readonly ProductsContext _context;
+        private readonly IConfiguration _configuration;
+
+        public ViewController(ProductsContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        // GET: View
+        public async Task<IActionResult> Index(int page=1)
+        {
+            List<Product> product = _context.Products.ToList();
+            const int pageSize = 5;
+            if (page < 1) page = 1;
+            int recCount = product.Count();
+            var pagination = new Pager(recCount, page, pageSize);
+            int recSkip =(page-1)*pageSize;
+            var data = product.Skip(recSkip).Take(pagination.PageSize).ToList();
+            this.ViewBag.Pager = pagination;
+            return View(data);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: View/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Code,Name,Category,Brand,Type,Description")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+        
+    }
+    
+}
---
+++ WebApplication1/Models/ProductsContext.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models;
+
+public partial class ProductsContext : DbContext
+{
+    public ProductsContext()
+    {
+    }
+
+    public ProductsContext(DbContextOptions<ProductsContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost,1433;Database=Products;User Id=sa;Password=15975321;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__product__3213E83FBE86E4DE");
+
+            entity.ToTable("product");
+
+            entity.HasIndex(e => e.Code, "UQ__product__357D4CF97DBBB7D0").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Brand)
+                .HasMaxLength(28)
+                .IsUnicode(false)
+                .HasColumnName("brand");
+            entity.Property(e => e.Category)
+                .HasMaxLength(28)
+                .IsUnicode(false)
+                .HasColumnName("category");
+            entity.Property(e => e.Code)
+                .HasMaxLength(9)
+                .IsUnicode(false)
+                .HasColumnName("code");
+            entity.Property(e => e.Description)
+                .HasMaxLength(180)
+                .IsUnicode(false)
+                .HasColumnName("description");
+            entity.Property(e => e.Name)
+                .HasMaxLength(90)
+                .IsUnicode(false)
+                .HasColumnName("name");
+            entity.Property(e => e.Type)
+                .HasMaxLength(21)
+                .IsUnicode(false)
+                .HasColumnName("type");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
